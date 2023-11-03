27c3090af68813482f037a880c234e9f629e2c52
elmurod <elmurodraimov764@gmail.com>
2023-11-03 11:58:37 +0500

    code

--- if03.py
+++ if03.py
@@ -1,10 +1,8 @@
 def main(a):
-    """
-    If the number is positive, increase it to 1,if the number is negative decrease it to 2.
-    If it is 0, assign 10.
-    Args:
-        a: integer
-    Returns:
-        a: integer
-    """
-    return
+    if a<0:
+        a -= 2
+    if a>0:
+        a +=1
+    return a
+print(main(-9))
+print(main(3))
