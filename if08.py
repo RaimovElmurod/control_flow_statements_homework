736369478a73043fc7c38b59ade80dd94f43f50f
elmurod <elmurodraimov764@gmail.com>
2023-11-05 18:26:20 +0500

    code

--- if08.py
+++ if08.py
@@ -1,14 +1,19 @@
 def main(a):
-    """
-    Given an integer a, check the following conditions:
     "two-digit odd number",
     "two-digit even number",
     "three-digit odd number",
     "three-digit even number"
 
-    Args:
-        a: integer
-    Returns:
-        string: the message to print
-    """
-    return
+    n=0
+    if a>0 and n%2 == 0:
+        print("two-digit odd number")
+    if a>0 and n%2 == 1:
+        print("two-digit even number")
+    if a<0 and n%2 == 1:
+        print("three-digit odd number")
+    if a<0 and n%2 == 0:
+        print("three-digit even number")
+    if a == 0:
+        return n
+print(main(57))
+print(main(-242))
